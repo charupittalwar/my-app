376bee07ab91ecddad2c429690a7f841185aebc6
charupittalwar <charu4pittalwar@gmail.com>
2023-05-18 15:55:56 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Add from './components/Add/Add';
 function App() {
   return (
     <div>
-    <h1 id ="c1">charu pittalwar</h1>
+    <h1 id ="c1">first project 1</h1>
+
+    <h2>first project 2</h2>
 
     
     <Add  a="2" b = "3"></Add>
